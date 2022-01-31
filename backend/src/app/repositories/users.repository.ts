dffe12f06e4a0b4a64cf8940fef1d8b8bef4b05d
saddam-satria <ardhisaddam@gmail.com>
2022-01-31 19:38:36 +0700

    modify refactor code

--- backend/src/app/repositories/users.repository.ts
+++ backend/src/app/repositories/users.repository.ts
@@ -1,8 +1,5 @@
 import UsersEntity from '../../entities/Users';
 import { EntityRepository, Repository } from 'typeorm';
-import { sign } from 'jsonwebtoken';
-import { hash, genSalt, compare } from 'bcrypt';
-import { IPayload } from '../utils/interface/payload.interface';
 
 @EntityRepository(UsersEntity)
 class UsersRepository extends Repository<UsersEntity> {
@@ -14,26 +11,6 @@ class UsersRepository extends Repository<UsersEntity> {
   }
   public findById(id: string): Promise<UsersEntity | undefined> {
     return this.findOne({ id });
-  }
-  public generateToken({ email, name, id }: IPayload): string {
-    const secret_token: any = process.env.SECRET_TOKEN;
-    return sign(
-      {
-        id,
-        name,
-        email,
-      },
-      secret_token
-    );
-  }
-  public async hashPassword(plain: string): Promise<string> {
-    const salt: Promise<string> | string = await genSalt(10);
-    const hashPassword: Promise<string> | string = await hash(plain, salt);
-
-    return hashPassword;
-  }
-  public async comparePassword(plain: string, hash: string): Promise<boolean> {
-    return await compare(plain, hash);
   }
 }
 
