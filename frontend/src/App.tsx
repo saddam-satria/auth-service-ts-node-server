04f17171b57f5fb637c88ace3e91d2bae2e5135c
saddam-satria <ardhisaddam@gmail.com>
2022-02-01 20:55:05 +0700

    modify add middleware auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,38 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { RequiredToken } from './pages/protected/RequiredToken';
+import { Authenticated } from './pages/protected/Authenticated';
 
 const App: React.FC = (): JSX.Element => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={
+              <RequiredToken>
+                <Home />
+              </RequiredToken>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Authenticated>
+                <Login />
+              </Authenticated>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <Authenticated>
+                <Register />
+              </Authenticated>
+            }
+          />
         </Routes>
       </Router>
     </>
