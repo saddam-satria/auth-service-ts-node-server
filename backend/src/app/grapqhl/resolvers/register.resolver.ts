7e6003c2dfb5df47edcc02e3d826cb062c6fdfe4
saddam-satria <ardhisaddam@gmail.com>
2022-02-02 15:09:47 +0700

    modify add error handling

--- backend/src/app/grapqhl/resolvers/register.resolver.ts
+++ backend/src/app/grapqhl/resolvers/register.resolver.ts
@@ -3,9 +3,9 @@ import { getCustomRepository } from 'typeorm';
 import AuthMutation from '../mutations/auth.mutation';
 import UsersRepository from '../../repositories/users.repository';
 import UsersEntity from '../../../entities/Users';
-import AuthQuery from '../queries/auth.query';
-import hashModule from '../../modules/hash.module';
 import jwtHelper from '../../helpers/jwt.helper';
+import AuthQuery from '../queries/auth.query';
+import { validate } from 'class-validator';
 
 @Resolver()
 class RegisterResolver {
@@ -16,7 +16,16 @@ class RegisterResolver {
     try {
       newUser.email = user.email;
       newUser.name = user.name;
-      newUser.password = await hashModule.hashPassword(user.password);
+      newUser.password = user.password;
+
+      const errors = (await validate(newUser)) as any;
+
+      if (errors.length > 0) {
+        errors.map((error: any) => {
+          if (error.property.includes('email')) throw 'email must valid';
+          if (error.property.includes('password')) throw 'password must have between 8-16 characters';
+        });
+      }
       const currentUser: Promise<UsersEntity> = usersRepo.save(newUser);
       const accessToken: string = jwtHelper.accessToken({ email: (await currentUser).email, name: (await currentUser).name, id: (await currentUser).id });
       const refreshToken: string = jwtHelper.refreshToken({ email: (await currentUser).email, name: (await currentUser).name, id: (await currentUser).id });
@@ -28,10 +37,11 @@ class RegisterResolver {
         refreshToken,
       };
     } catch (error) {
-      return {
-        status: 'success',
-        message: 'register successfully',
-      };
+      if (error.message || error.code === '23505') {
+        if (error.message.includes('duplicate key value violates unique constraint')) throw new Error('Email has been register');
+      }
+
+      throw new Error(error);
     }
   }
 }
