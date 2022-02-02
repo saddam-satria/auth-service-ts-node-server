b593a0a6e4bf4e5f47feea9b4bd40b6210158995
saddam-satria <ardhisaddam@gmail.com>
2022-02-02 22:02:32 +0700

    modify add refresh token resolver

--- backend/src/app/grapqhl/schema.ts
+++ backend/src/app/grapqhl/schema.ts
@@ -1,10 +1,11 @@
 import { buildSchema } from 'type-graphql';
 import LoginResolver from './resolvers/login.resolver.';
+import RefreshTokenResolver from './resolvers/refreshToken.resolver';
 import RegisterResolver from './resolvers/register.resolver';
 import UsersResolver from './resolvers/users.resolver';
 
 export default async () => {
   return await buildSchema({
-    resolvers: [LoginResolver, RegisterResolver, UsersResolver],
+    resolvers: [LoginResolver, RegisterResolver, UsersResolver, RefreshTokenResolver],
   });
 };
