fdcf189fece50bea94f05fb58799f27b6c0030d0
saddam-satria <ardhisaddam@gmail.com>
2022-02-02 22:12:04 +0700

    create custom hook get data

---
+++ frontend/src/hooks/getData.hook.ts
@@ -0,0 +1,47 @@
+import { useQuery, useLazyQuery } from '@apollo/client';
+import { useEffect } from 'react';
+import { refreshTokenQuery } from '../graphql/queries/refreshToken.query';
+import useAuth from '../hooks/auth.hook';
+
+const GetDataHook = (query: any) => {
+  const currentToken = useAuth() as any;
+
+  const { data, error } = useQuery(query, {
+    variables: {
+      accessToken: currentToken.accessToken,
+    },
+  });
+
+  const [getNewToken, response] = useLazyQuery(refreshTokenQuery);
+
+  useEffect(() => {
+    if (error && error.message) {
+      if (error.message.includes('token expired')) {
+        getNewToken({
+          variables: {
+            refreshToken: currentToken.refreshToken,
+          },
+        });
+      }
+      if (error.message.includes('invalid token')) {
+        localStorage.clear();
+      }
+    }
+  }, [error, currentToken.refreshToken, getNewToken, response.data]);
+
+  useEffect(() => {
+    if (response.data) {
+      localStorage.setItem(
+        'currentUser',
+        JSON.stringify({
+          accessToken: response.data.refreshToken.accessToken,
+          refreshToken: currentToken.refreshToken,
+        })
+      );
+    }
+  }, [response.data, currentToken.refreshToken]);
+
+  return data;
+};
+
+export default GetDataHook;
