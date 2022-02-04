ececa8f9bcb623b539c4fb41a934395ce2e4948b
saddam-satria <ardhisaddam@gmail.com>
2022-02-04 12:27:28 +0700

    modify add cookie session && passport session reason: google oauth2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import express, { Response, Request } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './app/config/router';
 import path from 'path';
@@ -8,6 +8,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import schema from './app/grapqhl/schema';
 import { createConnection } from 'typeorm';
+import cookieSession from 'cookie-session';
 
 import passport from './app/config/passport';
 
@@ -16,12 +17,26 @@ import passport from './app/config/passport';
   const PORT: number | string = process.env.PORT || 5000;
   config();
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL as string,
+      credentials: true,
+    })
+  );
   app.use('/static', express.static(path.join(__dirname, '../public')));
   app.use(express.json());
   app.use(routes);
 
+  app.use(
+    cookieSession({
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      keys: [process.env.SECRET_TOKEN as string],
+    })
+  );
+
   app.use(passport.initialize());
+  app.use(passport.session());
   app.get(
     '/google',
     passport.authenticate('google', {
@@ -32,18 +47,19 @@ import passport from './app/config/passport';
     '/google/redirect',
     passport.authenticate('google', {
       scope: ['email', 'profile'],
-    }),
-    (req: Request, res: Response) => {
-      const user: any = req.user;
-
-      res.json({ user }).status(200);
-    }
+      successRedirect: `${process.env.CLIENT_URL as string}/auth/google/redirect`,
+    })
   );
+
+  app.get('/google/data', (req: Request, res: Response) => {
+    res.send(req.user);
+  });
 
   const apolloServer = new ApolloServer({
     schema: await schema(),
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
     introspection: true,
+    context: ({ req }) => req.headers,
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app, path: '/graphql' });
